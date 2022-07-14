15d9d1c21d581a016fa06130bbb5ca267bf80cd1
Dustin Martin <d_man_24_3@hotmail.com>
2022-07-13 19:50:02 -0500

    post

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 
 const { animals } = require("./data/animals");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -55,13 +59,24 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+    console.log(__dirname);
+  return animal;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
-  if (result) {
-    res.json(result);
+  if (results) {
+    res.json(results);
   } else {
     res.send(404);
   }
@@ -70,6 +85,34 @@ app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   res.json(result);
 });
+
+app.post("/api/animals", (req, res) => {
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.listen(PORT, () => {
   console.log(`Api server now on port ${PORT}`);
 });
