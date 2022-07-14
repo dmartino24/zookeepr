98bba8f7c41af9f590f2e27f5271cd2fce1d222f
Dustin Martin <d_man_24_3@hotmail.com>
2022-07-14 14:18:43 -0500

    frontend working

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -84,6 +85,19 @@ app.get("/api/animals", (req, res) => {
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   res.json(result);
+});
+
+app.get('/', (req,res)=> {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post("/api/animals", (req, res) => {
